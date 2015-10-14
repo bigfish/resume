6b464762ed8635e6e184b122787f36e9581003aa
David Wilhelm <dewilhelm@gmail.com>
2015-10-13 21:48:14 -0400

    setup gulp flow

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var markdown = require('gulp-markdown');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+//compile all markdown files to html
+gulp.task('markdown', function() {
+    return gulp.src('markdown/*.md')
+        .pipe(markdown())
+        .pipe(gulp.dest('html'));
+});
+
+gulp.task('concat', function () {
+    return gulp.src('html/*.html')
+        .pipe(concat('index.html'))
+        .pipe(gulp.dest('dist'));
+});
+
+//convert sass to css, and inject into browser
+gulp.task('sass', function () {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('build', ['markdown', 'concat', 'sass']);
+
+//watch src dir for changes
+gulp.task('watch', function() {
+
+    //serve dist dir with browser sync
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+
+    gulp.watch('markdown/*', ['markdown']);
+    gulp.watch('html/*.html', ['concat']);
+    gulp.watch('scss/*', ['sass']);
+    gulp.watch('dist/index.html', browserSync.reload);
+});
+
+
+gulp.task('default', ['build', 'watch']);
---
+++ markdown/header.md
@@ -0,0 +1,9 @@
+<header>
+  <h1>David Wilhelm</h1>
+    <ul id="address">
+      <li> 20 Young St, Guelph, ON N1G 1M2 </li>
+      <li> 519 362 0453 (c) </li>
+      <li> dewilhelm@gmail.com </li>
+      <li> blog ! GitHub | LinkedIn </li>
+  </ul>
+</header>
